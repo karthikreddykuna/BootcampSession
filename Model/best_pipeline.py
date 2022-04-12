97cb50505181560991e63792405505a25c1179ca
root <root@56895a73ee11>
2022-04-12 13:38:26 +0000

    Model Deployment

--- Model/best_pipeline.py
+++ Model/best_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('BootcampSession/Model/prepared_data.csv')
@@ -10,7 +10,7 @@ training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
 # Average CV score on the training set was: 0.9513211014116288
-exported_pipeline = DecisionTreeClassifier(criterion="entropy", max_depth=4, min_samples_leaf=19, min_samples_split=14)
+exported_pipeline = KNeighborsClassifier(n_neighbors=40, p=1, weights="distance")
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
